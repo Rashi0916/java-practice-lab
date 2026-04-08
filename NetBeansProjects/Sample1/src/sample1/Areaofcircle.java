d2d3870c2e8fd93b4adff7ca2c45f24b6f526d17
Rashi0916 <rashiyadav1625@gmail.com>
2026-04-08 13:42:37 +0530

    first commit

---
+++ NetBeansProjects/Sample1/src/sample1/Areaofcircle.java
@@ -0,0 +1,22 @@
+package sample1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rashi
+ */
+public class Areaofcircle {
+    private static final double radius = 7;
+    public static void main(String[] args){
+        double parimeter = 2* Math.PI * radius;
+        double area = Math.PI * radius *radius;
+        System.out.println("parimeter of a circle is =" + parimeter);
+        System.out.println("Area of a circle is ="+ area);
+    }
+    
+    
+}
